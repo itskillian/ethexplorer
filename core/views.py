e728a964a9c79dc7ef56d746e910dfe730ccee0e
killian <killiancodes@gmail.com>
2024-06-14 20:45:15 -0400

    Add basic style and html

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
-from django.http import JsonResponse
+# from django.http import JsonResponse
 from django.shortcuts import render
-from django.views import View
+# from django.views import View
 
 from core.utils import get_eth_balance
 
